f1a92704a204e8f517910493c81a1a45328ec71b
MaxKichiy <6aJIa6oJI1336@gmail.com>
2020-08-05 13:04:34 +0300

    add sorting component, and filter UI functionality

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@
 export { default as Button } from './Button';
 export { default as Categories } from './Categories';
 export { default as Header } from './Header';
+export { default as SortPopup } from './SortPopup';
 
 // export { Button, Header, Categories };
