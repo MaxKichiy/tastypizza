a8e467394ad83bd55c3b44e4ff7f0f2123756e84
MaxKichiy <6aJIa6oJI1336@gmail.com>
2020-08-06 13:50:59 +0300

    add redxu-thunk to project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import React, { useEffect } from 'react';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router';
-import { setPizzas as setPizzasAction } from './redux/actions/pizzas';
-import axios from 'axios';
+import { fetchPizzas } from './redux/actions/pizzas';
 import { useDispatch } from 'react-redux';
 
 function App(props) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/pizzas')
-      .then((res) => dispatch(setPizzasAction(res.data)));
+    dispatch(fetchPizzas());
   }, []);
   return (
     <div className='wrapper'>
